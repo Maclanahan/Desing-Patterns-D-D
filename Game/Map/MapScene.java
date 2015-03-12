24b8d3351cbb6f711938c69194159213678865b9
Kevin Reynolds <kevinmr54@yahoo.com>
2015-03-11 23:57:43 -0700

    Basic map GUI implemented
    You can use wasd and arrow keys to navigate the dungeon. Only the
    movement is implemented so far, and for now I've left the two buttons
    alone for testing.

--- Game/Map/MapScene.java
+++ Game/Map/MapScene.java
@@ -1,5 +1,7 @@
 package Map;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import java.util.Observable;
 import java.util.Scanner;
 
@@ -19,11 +21,67 @@ public class MapScene extends Observable implements GameScene
 	private Text txt;
 	private int numVisited = 0;
 	
+	private char[][] floor;
+	private int current = 0, tileWidth = 5, tileHeight = 5;
+	private Rectangle[][] tiles;
+	private Rectangle playerPos;
+	private DungeonFactory df = new DungeonFactory();
+	private Floor[] floors = df.getFloors();
+
+	private Player player = new Player(floors[current]);
+	
 	public MapScene()
 	{
+		Scanner scan = new Scanner(System.in);
+		
 		root = new Group();
 		scene = new Scene(root, 640, 480, Color.WHITE);
 		
+		addFloor();
+		
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() 
+				{
+					public void handle(KeyEvent key)
+					{
+						if(checkWall(0,1,player) && (key.getText().equals("d") || key.getCode() == KeyCode.RIGHT))
+						{
+							player.setCol(player.getCol()+1);
+							playerPos.setX(player.getCol()*tileWidth);
+						}
+						
+						if(checkWall(0,-1,player) && (key.getText().equals("a") || key.getCode() == KeyCode.LEFT))
+						{
+							player.setCol(player.getCol()-1);
+							playerPos.setX(player.getCol()*tileWidth);
+						}
+						
+						if(checkWall(-1,0,player) && (key.getText().equals("w") || key.getCode() == KeyCode.UP))
+						{
+							player.setRow(player.getRow()-1);
+							playerPos.setY(player.getRow()*tileHeight);
+						}
+						
+						if(checkWall(1,0,player) && (key.getText().equals("s") || key.getCode() == KeyCode.DOWN))
+						{
+							player.setRow(player.getRow()+1);
+							playerPos.setY(player.getRow()*tileHeight);
+						}
+						
+						if (floor[player.getRow()][player.getCol()] == 'E')
+						{
+							current += 1;
+							
+							removeFloor();
+							addFloor();
+							
+						}
+						
+					}
+				});
+		
+		scan.close();
+		   
+		//EventHandler handler = new EventHandler
 		txt = new Text(100,100, "I'm The Map Scene!\n Visited " + numVisited + " times");
 		
 		root.getChildren().add(txt);
@@ -41,7 +99,7 @@ public class MapScene extends Observable implements GameScene
 			}
 		});
 		
-		button.xProperty().set(300);
+		button.xProperty().set(400);
 		button.yProperty().set(300);
 		
 		root.getChildren().add(button);
@@ -59,93 +117,70 @@ public class MapScene extends Observable implements GameScene
 			}
 		});
 		
-		button2.xProperty().set(300);
+		button2.xProperty().set(400);
 		button2.yProperty().set(100);
 		
 		root.getChildren().add(button2);
 		
 	}
 	
+	private boolean checkWall(int row, int col, Player player)
+	{
+		if ( floor[player.getRow()+row][player.getCol()+col] != 'X')
+			return true;
+		
+		return false;
+	}
+	
+	private void removeFloor()
+	{
+		int i, j;
+		
+		for ( i=0; i<tiles.length; i++ )
+			for ( j=0; j<tiles[0].length; j++ )
+			{
+				root.getChildren().remove(tiles[i][j]);
+			}
+		
+		root.getChildren().remove(playerPos);
+	}
+	
+	private void addFloor()
+	{
+		int i, j;
+		
+		tiles = new Rectangle[floors[current].getFloor().length][floors[current].getFloor()[current].length];
+		floor = floors[current].getFloor();
+		
+		for ( i=0; i<tiles.length; i++ )
+			for ( j=0; j<tiles[0].length; j++ )
+			{
+				if ( floor[i][j] == 'X' )
+					tiles[i][j] = new Rectangle(tileWidth,tileHeight,Color.BLACK);
+				else if ( floor[i][j] == 'E')
+					tiles[i][j] = new Rectangle(tileWidth,tileHeight,Color.RED);
+				else if ( floor[i][j] == 'B')
+					tiles[i][j] = new Rectangle(tileWidth,tileHeight,Color.ORANGE);
+				else
+					tiles[i][j] = new Rectangle(tileWidth,tileHeight,Color.WHITE);
+				
+				tiles[i][j].setX(j*tileWidth);
+				tiles[i][j].setY(i*tileHeight);
+				
+				root.getChildren().add(tiles[i][j]);
+			}
+		
+		player = new Player(floors[current]);
+		playerPos = new Rectangle(tileWidth, tileHeight,Color.GREEN);
+		playerPos.setX(player.getCol()*tileWidth);
+		playerPos.setY(player.getRow()*tileHeight);
+		root.getChildren().add(playerPos);
+	}
+	
 	@Override
 	public void Start() 
 	{
-		/*int i, j, current = 0;
-		   boolean play = true;
-		   char c;
-		   DungeonFactory df = new DungeonFactory();
-		   Scanner scan = new Scanner(System.in);
-		   String input;
-		   Floor[] floors = df.getFloors();
-		   char[][] copy;
-		  
-		   Player player = new Player(floors[current]);
-		   
-		   copy = new char[floors[current].getFloor().length][floors[current].getFloor()[current].length];
-		   for ( i=0; i<copy.length; i++)
-			   for ( j=0; j<copy[current].length; j++)
-				   copy[i][j] = floors[current].getFloor()[i][j];
-		   
-		   copy[player.getRow()][player.getCol()] = 'P';
-		   
-		   for ( i=0; i<copy.length; i++ )
-		   {
-			   for ( j=0; j<copy[current].length; j++ )
-				   System.out.print(copy[i][j]);
-			
-			   System.out.println("");
-		   }
-		   
-		   while ( play )
-		   {
-			   copy = new char[floors[current].getFloor().length][floors[current].getFloor()[current].length];
-			   for ( i=0; i<copy.length; i++)
-				   for ( j=0; j<copy[current].length; j++)
-					   copy[i][j] = floors[current].getFloor()[i][j];
-			   
-			   System.out.print("Enter direction(w,a,s,d):");
-			   input = scan.next();
-			   
-			   if ( input.equals("w") && copy[player.getRow()-1][player.getCol()] != 'X' )
-				   	player.setRow(player.getRow()-1);
-			   else if ( input.equals("s") && copy[player.getRow()+1][player.getCol()] != 'X')
-				   player.setRow(player.getRow()+1);
-			   else if ( input.equals("a") && copy[player.getRow()][player.getCol()-1] != 'X')
-				   player.setCol(player.getCol()-1);
-			   else if ( input.equals("d") && copy[player.getRow()][player.getCol()+1] != 'X')
-				   player.setCol(player.getCol()+1);
-			   else if ( input.equals("quit") )
-				   play = false;
-			   
-			   c = copy[player.getRow()][player.getCol()];
-			   copy[player.getRow()][player.getCol()] = 'P';
-		   
-			   if ( c == 'E')
-			   {
-				   current += 1;
-				   player = new Player(floors[current]);
-				   
-				   copy = new char[floors[current].getFloor().length][floors[current].getFloor()[current].length];
-				   for ( i=0; i<copy.length; i++)
-					   for ( j=0; j<copy[current].length; j++)
-						   copy[i][j] = floors[current].getFloor()[i][j];
-				   
-				   c = copy[player.getRow()][player.getCol()];
-				   copy[player.getRow()][player.getCol()] = 'P';
-			   }
-			   
-			   if ( c == 'B')
-				   play = false;
-				   
-			   for ( i=0; i<copy.length; i++ )
-			   {
-				   for ( j=0; j<copy[current].length; j++ )
-					   System.out.print(copy[i][j]);
-				
-				   System.out.println("");
-			   }
-		   }
-		   System.out.println("You won the game!");
-		   scan.close();*/
+		
 	}
 
 	@Override
